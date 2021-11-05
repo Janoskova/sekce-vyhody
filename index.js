b51798f9d288abed0ccdf6703bf5bc35ef7774dc
Eva Janoskova <janoskova.evca@gmail.com>
2021-11-05 12:42:58 +0100

    changes in section rates

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const showSavingsPerMonth = () => {
+const showSavingsPerMonth = (event) => {
   document.querySelector('.advantages__total').innerHTML = '1 896 Kč';
   document.querySelector('.advantages--premium').innerHTML =
     'Průměrná měsíční úspora člena Alza Premium';
   document.querySelector('.advantages--yearly').classList.remove('selected');
   document.querySelector('.advantages--monthly').classList.add('selected');
+  event.preventDefault();
 };
 
 document
   .querySelector('.advantages--monthly')
   .addEventListener('click', showSavingsPerMonth);
 
-const showSavingsPerYear = () => {
+const showSavingsPerYear = (event) => {
   document.querySelector('.advantages__total').innerHTML = '22 750 Kč';
   document.querySelector('.advantages--premium').innerHTML =
     'Průměrná roční úspora člena Alza Premium';
   document.querySelector('.advantages--monthly').classList.remove('selected');
   document.querySelector('.advantages--yearly').classList.add('selected');
+  event.preventDefault();
 };
 
 document
